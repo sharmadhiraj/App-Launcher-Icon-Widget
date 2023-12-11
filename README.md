7d8e71dade21f1a9ce51ef8f6027a067065b234c
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2023-12-11 10:50:04 +0545

    Add README.md

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # App Launcher Icon Widget
 
-Flutter plugin providing a widget and data for displaying the app launcher icon.
+Flutter plugin providing a widget and data for displaying the app launcher icon in Android & iOS.
 
-##### Work In Progress .....
+#### Installation Guide
 
+Check [here](https://pub.dev/packages/app_launcher_icon_widget/install)
+
+#### Usage
+
+##### Using the widget
+
+`import 'package:app_launcher_icon_widget/app_launcher_icon_widget.dart';
+
+// ...
+
+AppLauncherIcon(
+width: 64, //optional
+height: 64, //optional
+)`
+
+##### Using the method
+
+`import 'package:app_launcher_icon_widget/app_launcher_icon_widget.dart';
+
+// ...
+
+Uint8List? appIcon = await AppLauncherIconWidget.getAppLauncherIcon();`
--- lib/app_launcher_icon_widget.dart
+++ lib/app_launcher_icon_widget.dart
@@ -1,9 +1,41 @@
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+
 import 'app_launcher_icon_widget_platform_interface.dart';
 
 class AppLauncherIconWidget {
   static Future<Uint8List?> getAppLauncherIcon() {
     return AppLauncherIconWidgetPlatform.instance.getAppLauncherIcon();
+  }
+}
+
+class AppLauncherIcon extends StatelessWidget {
+  AppLauncherIcon({this.width = 64, this.height = 64});
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Uint8List?>(
+      future: AppLauncherIconWidget.getAppLauncherIcon(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData &&
+            snapshot.data != null &&
+            snapshot.data!.isNotEmpty) {
+          return Image.memory(
+            snapshot.data!,
+            width: width,
+            height: height,
+          );
+        } else {
+          return Container(
+            width: width,
+            height: height,
+          );
+        }
+      },
+    );
   }
 }
