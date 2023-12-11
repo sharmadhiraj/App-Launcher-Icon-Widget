58aa8a1aa8a1b277d51af710bebf94713b3cb7ff
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2023-12-11 10:40:56 +0545

    Implement functionality in iOS.

--- ios/Classes/AppLauncherIconWidgetPlugin.swift
+++ ios/Classes/AppLauncherIconWidgetPlugin.swift
@@ -2,18 +2,33 @@ import Flutter
 import UIKit
 
 public class AppLauncherIconWidgetPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "app_launcher_icon_widget", binaryMessenger: registrar.messenger())
-    let instance = AppLauncherIconWidgetPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "app_launcher_icon_widget", binaryMessenger: registrar.messenger())
+        let instance = AppLauncherIconWidgetPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "getAppLauncherIcon" {
+            result(getAppLauncherIcon())
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func getAppLauncherIcon() -> FlutterStandardTypedData? {
+        if let icon = getIconImage() {
+            return FlutterStandardTypedData(bytes: icon)
+        } else {
+            return nil
+        }
+    }
+
+    private func getIconImage() -> Data? {
+        if let icon = UIImage(named: "AppIcon") {
+            return icon.pngData()
+        } else {
+            return nil
+        }
+    }
 }
